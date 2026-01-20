3c60a8754e2e9fa4a11557a7e684ad6596fd5c5b
0anishpandit <0anishpandit@gmail>
2026-01-20 22:32:11 +0545

    Day 4 started

---
+++ 100 Days of Python Bootcamp Practise Exercise  /Day 4 /hear_tail_game.py
@@ -0,0 +1,9 @@
+#Head or Tail Game
+import random
+
+head_or_tail = random.randint(0,1)
+if head_or_tail == 1 :
+     print("Tail")
+else:
+     print("Head")
+
---
+++ 100 Days of Python Bootcamp Practise Exercise  /Day 4 /random_number_generation.py
@@ -0,0 +1,10 @@
+#first import the random module
+import random
+random_integer=random.randint(1,10)
+print(random_integer)
+
+random_float_0_to_1 = random.random()
+print(random_float_0_to_1)
+
+random_float = random.uniform(1,10)
+print(random_float)
